4a71e726169bfacb44f0d87ede8dbdffee7d79e6
origin <git@github.com:OljaKn/job4j_design.git>
2023-12-09 11:08:44 +0300

    Что такое обобщенные типы (generics)

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -26,7 +26,6 @@ public class Generics {
 
         generics.printLowerBoundedWildCard(first);
         generics.printLowerBoundedWildCard(second);
-       // generics.printLowerBoundedWildCard(third);
     }
 
     public void printObject(List<?> list) {
