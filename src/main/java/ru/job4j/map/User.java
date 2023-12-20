e012efedf416432fca269355b7b1ec49331537c6
origin <git@github.com:OljaKn/job4j_design.git>
2023-12-20 22:21:59 +0300

    User

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,4 +1,8 @@
 package ru.job4j.map;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -9,5 +13,32 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        User user = (User) obj;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        Calendar birthday = Calendar.getInstance();
+        Map<User, Object> map = new HashMap<>();
+        User user1 = new User("Masha", 2, birthday);
+        User user2 = new User("Masha", 2, birthday);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
     }
 }
