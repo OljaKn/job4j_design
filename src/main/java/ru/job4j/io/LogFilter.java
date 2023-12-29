c6f480d2009ce1fd1cf83eaaeed7090f6b2b5137
origin <git@github.com:OljaKn/job4j_design.git>
2023-12-29 15:46:51 +0300

    BufferedReader

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    private final String file;
+
+    public LogFilter(String file) {
+        this.file = file;
+    }
+
+    public List<String> filter() {
+        List<String> rsl = new ArrayList<>();
+        try (BufferedReader input = new BufferedReader(new FileReader("data/log.txt"))) {
+            rsl = input.lines()
+                    .filter(line -> line.contains(" 404 "))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter("data/log.txt");
+        logFilter.filter().forEach(System.out::println);
+    }
+
+}
