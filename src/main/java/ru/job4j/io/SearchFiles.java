5f0855fc5881b7aa2b7598942179e2fb59c713c6
origin <git@github.com:OljaKn/job4j_design.git>
2024-01-05 22:07:22 +0300

    Сканирование файловой системы

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    List<Path> paths;
+    Predicate<Path> condition;
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.paths = new ArrayList<>();
+        this.condition = condition;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (condition.test(file)) {
+            paths.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
