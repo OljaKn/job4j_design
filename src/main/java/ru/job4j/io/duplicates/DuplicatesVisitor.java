87eb8b9629f12031c7ded9aa8786cf05ea098a1b
origin <git@github.com:OljaKn/job4j_design.git>
2024-01-08 17:02:31 +0300

    Поиск дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -2,31 +2,34 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private Map<FileProperty, Path> propertyMap;
-    private List<FileProperty> paths;
+    private Map<FileProperty, List<Path>> propertyMap;
 
     public DuplicatesVisitor() {
         this.propertyMap = new HashMap<>();
-        this.paths = new ArrayList<>();
     }
 
-    public List<FileProperty> getPaths() {
-        return paths;
+    public List<Path> getPropertyMap() {
+        return new ArrayList<>();
     }
+
     @Override
     public FileVisitResult visitFile(Path file,
                                      BasicFileAttributes attributes) throws IOException {
         FileProperty fileProperty = new FileProperty(attributes.size(), file.getFileName().toString());
+        List<Path> duplicate = new ArrayList<>();
        if (!propertyMap.containsKey(fileProperty)) {
-           paths.add(fileProperty);
+           propertyMap.put(fileProperty, duplicate);
+           duplicate.add(file.toAbsolutePath());
        } else {
-           propertyMap.put(fileProperty, file);
+           duplicate.add(file.toAbsolutePath());
+           propertyMap.put(fileProperty, duplicate);
        }
         return super.visitFile(file, attributes);
     }
