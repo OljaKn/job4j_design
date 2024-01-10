b6f1d8e1268766bd75ad100158944d043326195b
origin <git@github.com:OljaKn/job4j_design.git>
2024-01-10 12:17:31 +0300

    Slf4j - вывод exception.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -12,21 +12,14 @@ public class UsageLog4j {
         LOG.debug("debug message");
         LOG.info("info message");
         LOG.warn("warn message");
-        LOG.error("error message");*/
+        LOG.error("error message");
         String name = "Petr Arsentev";
         int age = 33;
-        LOG.debug("User info name : {}, age : {}", name, age);
-
-        byte b = 12;
-        short sh = 123;
-        int in = -123;
-        long l = 4748588;
-        float f = 75.5f;
-        double d = 12.54;
-        char ch = 'a';
-        boolean boo = true;
-        LOG.debug("Log info byte : {}, short : {}, int : {}, long : {},"
-                        + " float : {}, double : {}, char : {}, boolean : {}",
-                b, sh, in, l, f, d, ch, boo);
+        LOG.debug("User info name : {}, age : {}", name, age);*/
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
