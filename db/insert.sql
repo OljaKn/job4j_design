072e94134b5e078573da029593820ad3eb9dfe0b
origin <git@github.com:OljaKn/job4j_design.git>
2024-01-14 17:42:08 +0300

    Связи между таблицами

--- db/insert.sql
+++ db/insert.sql
@@ -1,1 +1,12 @@
-insert into peoples(name, area, phoneNumber) values('Vasya', 'Moscow', '143243');
+create table books(
+    id serial primary key,
+    authorsWork varchar(255),
+    position_id int references Author(id)
+);
+
+insert into Author(name) values ('Достоевский');
+insert into books(authorsWork, Author_id) VALUES ('Бесы', 1);
+
+select * from books;
+
+select * from Author where id in (select Author_id from books);
