6335da570b7b9944b4b8919a2ca8ff2a04c921b3
origin <git@github.com:OljaKn/job4j_design.git>
2024-01-15 22:08:35 +0300

    Архивировать проект

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,21 +1,34 @@
 package ru.job4j.io;
 
 import java.io.*;
+
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+
 public class Zip {
 
-    public void packFiles(List<Path> sources, File target) {
-
+    public void packFiles(List<Path> sources, Path target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(String.valueOf(target))))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toString()));
+                try (BufferedInputStream output = new BufferedInputStream(new FileInputStream(path.toString()))) {
+                    zip.write(output.readAllBytes());
+                }
+                zip.closeEntry();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+        public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(String.valueOf(target))))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream output = new BufferedInputStream(new FileInputStream(source))) {
+            try (BufferedInputStream output = new BufferedInputStream(new FileInputStream(String.valueOf(source)))) {
                 zip.write(output.readAllBytes());
             }
         } catch (Exception e) {
@@ -23,11 +36,17 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
+        ArgsName arguments = ArgsName.of(args);
+        String directory = arguments.get("d");
+        String exclude = arguments.get("e");
+        String output = arguments.get("o");
+
+        File target = new File(output);
+        File dir = new File(directory);
+        List<Path> files = new Search().search(Paths.get(directory), file -> !file.toString().endsWith(exclude));
+
+        zip.packFiles(files, target.toPath());
     }
 }
