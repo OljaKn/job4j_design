df7c55cc3a35b6098db6756778fb18ff3a041536
origin <git@github.com:OljaKn/job4j_design.git>
2024-01-27 21:47:00 +0300

    Выполнить подзапросы

---
+++ db/Connection 13.sql
@@ -0,0 +1,35 @@
+CREATE TABLE customers(
+    id         serial primary key,
+    first_name text,
+    last_name  text,
+    age        int,
+    country    text
+);
+insert into customers (first_name, last_name, age, country) values ('Olja', 'Knyazeva', 32, 'Russia');
+insert into customers (first_name, last_name, age, country) values ('Oleg', 'Ivanov', 40, 'USA');
+insert into customers (first_name, last_name, age, country) values ('Vasya', 'Petrov', 30, 'China');
+insert into customers (first_name, last_name, age, country) values ('Petya', 'Vasiliev', 18, 'Russia');
+insert into customers (first_name, last_name, age, country) values ('Fedya', 'Zyev', 29, 'Italia');
+
+select * from customers 
+where age = (select min(age) from customers);
+
+CREATE TABLE orders(
+    id          serial primary key,
+    amount      int,
+    customer_id int references customers (id)
+);
+
+insert into orders (amount, customer_id) values (2, 1);
+insert into orders (amount, customer_id) values (3, 2);
+insert into orders (amount, customer_id) values (1, 3);
+insert into orders (amount, customer_id) values (10, 4);
+insert into orders (amount, customer_id) values (5, 5);
+insert into orders (amount, customer_id) values (4, null);
+insert into orders (amount, customer_id) values (null, null);
+update orders set amount = null where amount = 10;
+update orders set customer_id = null where amount = 3;
+
+select * from customers 
+where customers.id not in (select customer_id from orders);
+
