0f4933999ab5e6a5c4a391cee3ffdad23103bd17
origin <git@github.com:OljaKn/job4j_design.git>
2024-01-29 21:55:01 +0300

    JDBC

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -9,23 +9,11 @@ import java.sql.SQLException;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/idea_db";
-        String login = "postgres";
-        String password = "password";
-        try (Connection connection = DriverManager.getConnection(url, login, password)) {
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getUserName());
-            System.out.println(metaData.getURL());
-        }
-        Class.forName("org.postgresql.Driver");
-        Config config = new Config("./app.properties");
+        Config config = new Config("./src/main/resources/app.properties");
         config.load();
-        url = config.value("url");
-        login = config.value("login");
-        password = config.value("password");
         Class.forName("org.postgresql.Driver");
-        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+        try (Connection connection = DriverManager.getConnection(
+                config.value("url"), config.value("login"), config.value("password"))) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
             System.out.println(metaData.getURL());
