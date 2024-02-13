66b3225e7fcd86ff74407d18bc9a9b9572ee4afe
origin <git@github.com:OljaKn/job4j_design.git>
2024-02-13 22:35:36 +0300

    Принципы Kiss, Dry и Yagni

--- src/test/java/ru/job4j/kiss/fool/FoolTest.java
+++ src/test/java/ru/job4j/kiss/fool/FoolTest.java
@@ -1,10 +1,6 @@
 package ru.job4j.kiss.fool;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Scanner;
-
-import static java.lang.System.in;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
